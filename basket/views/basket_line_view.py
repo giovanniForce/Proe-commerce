48c698a73a7ed9624f39dd93eb521b3ee66399a1
Ndayou Ronald Vincent <vincent_nronald@yahoo.com>
2023-04-04 16:06:01 +0100

    attempt to improve basket views

--- basket/views/basket_line_view.py
+++ basket/views/basket_line_view.py
@@ -8,3 +8,12 @@ class LineView(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = BasketLineSerializer
     queryset = Line.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
--- basket/views/basket_view.py
+++ basket/views/basket_view.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, viewsets, permissions
-from basket.serializers import BasketSerializer
-from basket.models import Basket
+from rest_framework import generics, viewsets, permissions, response, status
+from basket.serializers import BasketSerializer, BasketLineSerializer
+from rest_framework.decorators import action
+from basket.models import Basket, Line
 
 class BasketView(viewsets.ModelViewSet):
     """
@@ -8,4 +9,14 @@ class BasketView(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = BasketSerializer
     queryset = Basket.objects.all()
-    
+
+    @action(detail=True, methods=['get'])
+    def get_line(self, request, *args, **kwargs):
+        '''
+            getting lines related to a basket
+        '''
+        
+        object = self.get_object()
+        data = Line.objects.filter(basket=object)
+        serializer = BasketLineSerializer(data=data, many=True)
+        return response.Response(status=status.HTTP_200_OK, data=serializer.data)
